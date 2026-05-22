80adbd2ac1d99f126de6b4a3df384b1f8c19fc93
Kimboleh <kimberlynachbur@gmail.com>
2026-05-22 15:26:41 -0700

    update asset URLs

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -5,24 +5,24 @@ export default function Home() {
     return (
         <div>
             <HeaderNav
-                logo='../src/img/favicon.ico'
+                logo='/favicon.ico'
                 siteTitle={''}
                 button={<Button primary rounded={false} link={`/connect`} label='Contact Me' />}
             />
-            { /* TOP HERO */ }
+            { /* TOP HERO */}
             <section id="main-hero" aria-label="introduction">
                 <div id="hero-left">
                     <div id="hero-title">
                         <h1>Hey there.</h1>
-                        <h1>I'm Kimberly, <br/><span id="description-tags"><span id="webdev">web developer</span>, <span id="swe">software engineer</span>, & <span id="uxd">WordPress wizard</span>.</span></h1>
+                        <h1>I'm Kimberly, <br /><span id="description-tags"><span id="webdev">web developer</span>, <span id="swe">software engineer</span>, & <span id="uxd">WordPress wizard</span>.</span></h1>
                     </div>
                 </div>
                 <div id="hero-right">
-                    <img src="src/img/hero-image.webp" alt="Kimberly, a white woman with curly red hair, stands in a forest next to a trailhead sign. She is looking to the left, and she is wearing a green beanie, black rain jacket, black jeans, and a graphic t-shirt." />
+                    <img src="/assets/img/hero-image.webp" alt="Kimberly, a white woman with curly red hair, stands in a forest next to a trailhead sign. She is looking to the left, and she is wearing a green beanie, black rain jacket, black jeans, and a graphic t-shirt." />
                     <div id="photo-credit" aria-label="photo credit">&#169; Blaine Greenway 2024</div>
                 </div>
             </section>
-            { /* ABOUT ME HERO */ }
+            { /* ABOUT ME HERO */}
             <section id="about-me" aria-label="About Me">
                 <div id="about-left">
                     <div id="about-text">
@@ -40,12 +40,12 @@ export default function Home() {
                                 <li>JavaScript & TypeScript</li>
                                 <li>React.js</li>
                                 <li>Node</li>
-                                <li>WordPress Development</li><br/>
+                                <li>WordPress Development</li><br />
 
                                 <li>HTML/CSS</li>
                                 <li>SCSS & SASS</li>
                                 <li>PHP</li>
-                                <li>Web Content Accessibility Guidelines (WCAG)</li><br/>
+                                <li>Web Content Accessibility Guidelines (WCAG)</li><br />
 
                                 <li>Java</li>
                                 <li>Kotlin</li>
@@ -55,14 +55,14 @@ export default function Home() {
                             <ul id="list-2" className="inactive-list">
                                 <li>Git & SVN</li>
                                 <li>GitHub</li>
-                                <li>Bitbucket</li><br/>
+                                <li>Bitbucket</li><br />
 
                                 <li>Storybook</li>
                                 <li>Figma</li>
-                                <li>AdobeXD</li><br/>
+                                <li>AdobeXD</li><br />
 
                                 <li>User Experience Design</li>
-                                <li>Design Systems</li><br/>
+                                <li>Design Systems</li><br />
 
                                 <li>Public Speaking & Presenting</li>
                                 <li>Communicating to Diverse Audiences</li>
@@ -71,22 +71,21 @@ export default function Home() {
                         </div>
                     </div>
                     <div id="trees">
-                        <img src="../src/img/tree-fill.svg" alt="taller silhouette of a pine tree" id="tree-1"/><img src="../src/img/tree-fill.svg" alt="shorter silhouette of a pine tree" id="tree-2"/>
+                        <img src="assets/img/tree-fill.svg" alt="taller silhouette of a pine tree" id="tree-1" /><img src="assets/img/tree-fill.svg" alt="shorter silhouette of a pine tree" id="tree-2" />
                     </div>
                 </div>
             </section>
-            { /* PROJECTS */ }
+            { /* PROJECTS */}
             <section id="projects-section" aria-label="My Projects">
                 <h2>projects I've worked on!</h2>
                 <div id="project-cards">
-                    { /* Cards added in via index.js function */ }
-                    <script type="application/javascript" src="../src/js/project-cards.js"></script> 
+                    { /* Cards added in via JS function */}
                 </div>
             </section>
             <footer id="footer">
                 <div className="icon-bar">
-                    <a href="https://github.com/kimboleh"><img src="src/img/icons/github.png" alt="github logo icon" /></a>
-                    <a href="mailto:hello@kimberlynachbur.dev"><img src="src/img/icons/email.png" alt="email envelope icon" /></a>
+                    <a href="https://github.com/kimboleh"><img src="assets/img/icons/github.png" alt="github logo icon" /></a>
+                    <a href="mailto:hello@kimberlynachbur.dev"><img src="assets/img/icons/email.png" alt="email envelope icon" /></a>
                 </div>
                 <p className="credits">Coded by hand with HTML, CSS, & JavaScript.</p>
                 <p className="credits">&#169; K.N. 2026</p>
