22d552095b48178c9ad91ef553ff6aa0a39cb0cb
Kimboleh <kimberlynachbur@gmail.com>
2026-05-22 15:26:14 -0700

    add URL redirection for routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import App from './App'
 
+// Restore path from GitHub Pages 404 redirect
+const redirect = new URLSearchParams(window.location.search).get('redirect');
+if (redirect) {
+  window.history.replaceState(null, '', redirect);
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     //<React.StrictMode>
     <BrowserRouter>
